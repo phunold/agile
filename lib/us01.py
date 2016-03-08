822ca4c2912fa35e036ce4031974436cd417f778
Ezhilarasi Swaminathan <ezhilarasi.swaminathan@gmail.com>
2016-03-08 00:20:36 -0500

    US01 and US27

---
+++ lib/us01.py
@@ -0,0 +1,58 @@
+"""
+US01: Don't permit Dates before current date    
+Author: Ezhilarasi Swaminathan
+
+"""
+
+from datetime import datetime
+
+def check(families, individuals):
+  
+  personDates = dict() # stores deat/birth dates
+  familyDates = dict()
+  id = '' # remembers ID to link dates back to individual 
+  today = datetime.date(datetime.now())
+  
+  # extract MARR and DIVO date from families
+  for index,i in enumerate(families):
+    tag = i['tag'] 
+    arg = i['arg']
+    
+    # memorize ID
+    if tag == 'FAM':
+        id = arg
+        familyDates[id] = dict()
+        
+    if tag == 'DATE':
+        date_string = arg
+        familyDates[id][tag] = datetime.date(datetime.strptime(date_string, "%d %b %Y"))
+  
+  # extract BIRTH and DEAT dates from individuals
+  for index,i in enumerate(individuals):
+    tag = i['tag'] 
+    arg = i['arg'] 
+
+    # memorize ID
+    if tag == 'INDI':
+      id = arg
+      personDates[id] = dict()
+
+    if tag == 'DATE':
+      date_string = arg
+      personDates[id][tag] = datetime.date(datetime.strptime(date_string, "%d %b %Y"))
+  
+  #See if there are dates post the current date/including current date  
+  for id,dateItem in personDates.items():
+     if dateItem['DATE'] >= today:
+        print 'US01 Only dates before current date allowed'
+        print 'Birth/Death recorded in the Gedcom file should be prior to today'
+        print '>>Error for',id,'with birth/death date',dateItem['DATE']
+
+  for id,dateItem in familyDates.items():
+     if dateItem['DATE'] >= today:
+        print 'US01 Only dates before current date allowed'
+        print 'Marriage/Divorce recorded in the Gedcom file should be prior to today'
+        print '>>Error for',id,'with marriage/divorce date',dateItem['DATE']
+    
+
+        
---
+++ lib/us27.py
@@ -0,0 +1,53 @@
+"""
+US27: Include inidvidual Age in the report 
+Author: Ezhilarasi Swaminathan
+
+"""
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+dates = dict() # stores deat/birth dates
+names = dict()
+ages = dict()
+id = '' # remembers ID to link dates back to individual 
+today = datetime.now()
+
+
+def display(individuals):
+
+  # extract BIRTH and DEAT dates from individuals
+  for index,i in enumerate(individuals):
+    tag = i['tag'] 
+    arg = i['arg'] 
+
+    # memorize ID
+    if tag == 'INDI':
+      id = arg
+      dates[id] = dict()
+      
+    if tag == 'NAME':
+      names [id] = arg
+       
+    if tag == 'DEAT' or tag == 'BIRT':
+      # get date from next item
+      date_record = individuals[index + 1]
+      date_string = date_record['arg'] 
+      dates[id][tag] = datetime.strptime(date_string, "%d %b %Y")
+     
+  for id,date in dates.items():
+    if 'BIRT' in date and 'DEAT' in date:
+        ages[id] = abs(( date['DEAT'] - date['BIRT']).years)
+    elif 'BIRT' in date and not 'DEAT' in date:
+        ages[id] = relativedelta(today, date['BIRT']).years
+  
+  print '*'*50
+  print '\tIndividuals Age Report'
+  print '*'*50    
+  print 'NAME\t\t\tAGE(years)\n'  
+  width = 30
+  for id,age in ages.items():
+      for id1,name in names.items():
+        if id == id1:
+            print "{} {}".format(name.ljust(width),age)  
+  print '*'*50    
