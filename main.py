bb39a460b1d547a88798c34f2cb9f2823a1cdd51
Ezhilarasi Swaminathan <ezhilarasi.swaminathan@gmail.com>
2016-03-08 00:39:16 -0500

    US01 and US27

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+from lib import *
+
+if len(sys.argv) != 2:
+  print("USAGE: %s <GEDCOM FILE>" % sys.argv[0])
+  sys.exit()
+
+ged = sys.argv[1]
+
+families, individuals = utils.load_from_file(ged)
+
+# Run Checks
+us02.check(individuals)
+us03.check(individuals)
+us13.check(families, individuals)
+us22.check(families, individuals)
+us07.check(individuals)
+us21.check(families, individuals)
+us01.check(families, individuals)
+
+#Display Reports
+us27.display(individuals)
