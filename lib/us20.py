81dd07d1b43ddc296911b0a3bafce131ecfc9988
Anas Amoudi <anasete1@gmail.com>
2016-04-19 18:13:18 -0400

    Added us20

--- lib/us20.py
+++ lib/us20.py
@@ -4,3 +4,44 @@ US20 Aunts and uncles
 Author: Anas Alamoudi
 
 """
+
+
+def display(families, individuals):
+  indi = dict()
+  for index,i in enumerate(individuals):
+    tag = i['tag'] 
+    arg = i['arg'] 
+
+    if tag == 'INDI':
+      id = arg
+      indi[id] = arg
+    if tag == 'NAME':
+      indi[id] = arg
+  
+  married = dict()
+  kids = dict()
+  for index, i in enumerate(families):
+    # get all married people in one list
+    tag = i['tag'] 
+    arg = i['arg'] 
+    if tag == 'FAM':
+      id = arg
+      married[id] = list()
+      kids[id] = list()
+    if tag == 'HUSB' or tag =='WIFE':
+      married[id].append(arg)
+    # add children from each family in one list
+    if tag == 'CHIL':
+      kids[id].append(arg)
+  
+  print ('*'*50)
+  print ('\tAunts and uncles')
+  print ('*'*50    )
+  print ('ID\t\tNAME\t\t\n'  )
+  WIDTH = 15
+  # check if their parents are in one list in the children lists  
+  for key in married.keys():
+  	for i in married[key]:
+  		print ("{} {}".format(i.ljust(WIDTH),
+          indi[i].ljust(WIDTH)))
+  print ('*'*50)
